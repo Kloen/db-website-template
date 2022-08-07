fd1f72c27b0a2d0f28c05f23bff77fb3aec08723
Kloen Lansfiel <kloen@kloenlansfiel.com>
2022-08-07 17:32:34 +0200

    Add nodemon, tailwindcss to prod, initial layouts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: 'class',
-    content: ["./views/**/*.art"],
+    content: ["./views/**/*.pug"],
     theme: {
         extend: {},
     },
