3e33ffb4777ac39a8e92fcfc4d37d1b839fb7d1b
Kloen Lansfiel <kloen@kloenlansfiel.com>
2022-08-07 18:26:10 +0200

    Add nav, locales, and some menu configurations.

--- app.js
+++ app.js
@@ -27,12 +27,14 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  console.log(err.message)
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('screens/error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const data = require('../data/data')
+const locales = require('../data/locales')
+
+router.get('/', function (req, res, next) {
+    res.render('screens/index', {nav: data.nav, locales: locales.supported});
 });
 
 module.exports = router;
